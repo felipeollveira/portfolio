f5cf1f8b3b46b69a264a094b3d1c40ca476c4930
Felipe Jesus <110308764+felipeollveira@users.noreply.github.com>
2023-08-30 21:02:13 -0300

    1 commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const about = document.getElementById('aboutme')
+about.textContent = 'Sempre muito curioso com as novas tecnologias do mundo, acredito que o talento sem o devido esforço não seja tão efetivo.'
+
+const email = document.getElementById('email')
+email.textContent = 'felipejesus411@yahoo.com.br'
